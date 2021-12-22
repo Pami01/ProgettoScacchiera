614cf9adfefacfeec238bf1ac974c377b0375319
Bovo01 <47921869+Bovo01@users.noreply.github.com>
2021-12-22 21:16:16 +0100

    Aggiunti metodi da implementare nell'header Board

--- include/Board.h
+++ include/Board.h
@@ -8,6 +8,22 @@
 
 namespace Chess
 {
+   enum Ending {
+      // false se non è finita la partita
+      NONE = 0,
+      // patte (1 - 5)
+      STALEMATE = 1,
+      _50_MOVE_RULE,
+      INSUFFICIENT_MATERIAL,
+      REPETITION,
+      AGREEMENT,
+      // Vittorie (10 - 11)
+      // Il bianco ha mattato
+      WHITE_CHECKMATE = 10,
+      // Il nero ha mattato
+      BLACK_CHECKMATE
+   };
+
    class Board
    {
    private:
@@ -17,6 +33,10 @@ namespace Chess
    private:
       // Prepara la posizione iniziale riempiendo il vector _pieces
       void initialize(void);
+      // Cambia il turno
+      void toggle_turn(void);
+      // Cerca il pezzo ad una certa posizione e lo ritorna
+      Piece find_piece(const Position &position);
 
    public:
       // Costruttore che inizializza una partita
@@ -27,9 +47,17 @@ namespace Chess
       // Eccezione per indicare una mossa invalida o illegale
       class InvalidMoveException;
 
+      // Getter per i pezzi del bianco
+      std::vector<Piece> pieces_white(void) const;
+      // Getter per i pezzi del nero
+      std::vector<Piece> pieces_black(void) const;
+
       // Sposta un pezzo dalla posizione 'from' alla posizione 'to'
       // Lancia una 'InvalidMoveException', se per qualche motivo la mossa non è valida
       void move(const Position &from, const Position &to);
+
+      // Ritorna 'Ending::NONE = 0' se la partita non è finita, altrimenti ritorna il modo in cui è finita la partita
+      Ending is_game_over(void) const;
 
       // Stampa la scacchiera
       friend std::ostream &operator<<(std::ostream &os, const Board &b);
