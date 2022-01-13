1b04b4620c2035243c7db4a68f9dfff1b52eef25
y0matt <mattiabertoldo1@gmail.com>
2022-01-13 11:24:19 +0100

    Sviluppo classe position

--- include/Position.h
+++ include/Position.h
@@ -7,14 +7,14 @@ namespace Chess
 {
    struct Position
    {
-   private:
-      short _x, _y;
+   public:
+      short x, y;
 
    private:
       // Controlla se l'oggetto è valido (x e y compresi tra 0 e 7)
       bool is_valid(void) const;
       // Converte la notazione aritmetica (A1, B7, H6, ...) e inizializza le variabili private
-      //    alle coordinate corrette ({0, 0}, {1, 6}, {7, 5}, ...)
+      //    alle coordinate corrette ({0, 0}, {6, 1}, {5, }, ...)
       // Lancia una 'InvalidPositionException' se la posizione inserita non è valida
       void decode_arithmetic_notation(const std::string &arithmetic_notation);
 
@@ -35,9 +35,9 @@ namespace Chess
       Position move(const short x_increment, const short y_increment) const;
 
       // Operatore di uguaglianza
-      bool operator==(const Position &other);
+      bool operator==(const Position &other) const;
       // Operatore di disuguaglianza
-      bool operator!=(const Position &other);
+      bool operator!=(const Position &other) const;
    };
 }
 
